1dd160865e382ba1c27ca34bb8baf37ad86203d1
Brian N. West <brian.n.west@gmail.com>
2017-03-25 12:06:33 -0400

    initial release. retrieve loved tracks and deserialize them.

---
+++ LastFmRecommendations/LastFm.cs
@@ -0,0 +1,454 @@
+﻿//
+// How to Deserialize XML document:
+//
+// http://stackoverflow.com/questions/364253/how-to-deserialize-xml-document/19613934#19613934
+//
+// Method 1 - XSD tool
+// 1. Open Developer Command Prompt: %comspec% /k ""C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat""
+// 2. Change location to your XML file directory by typing cd /D "C:\path\to\xml"
+// 3. Create XSD file (file.xsd) from your xml file by typing:    xsd file.xml
+// 4. Create C# classes (file.cs) by typing:                      xsd /c file.xsd
+//
+// Method 2 - Paste Special
+// 1. Copy content of your XML file to clipboard
+// 2. Add to your solution new, empty class file (Shift+Alt+C)
+// 3. Open that file and in menu click Edit > Paste special > Paste XML As Classes
+//
+// Used Method 2.
+//
+
+namespace LastFmRecommendations.LovedTracks
+{
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+    public partial class lfm
+    {
+
+        private lfmLovedtracks lovedtracksField;
+
+        private string statusField;
+
+        /// <remarks/>
+        public lfmLovedtracks lovedtracks
+        {
+            get
+            {
+                return this.lovedtracksField;
+            }
+            set
+            {
+                this.lovedtracksField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string status
+        {
+            get
+            {
+                return this.statusField;
+            }
+            set
+            {
+                this.statusField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class lfmLovedtracks
+    {
+
+        private lfmLovedtracksTrack[] trackField;
+
+        private string userField;
+
+        private byte pageField;
+
+        private byte perPageField;
+
+        private byte totalPagesField;
+
+        private ushort totalField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("track")]
+        public lfmLovedtracksTrack[] track
+        {
+            get
+            {
+                return this.trackField;
+            }
+            set
+            {
+                this.trackField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string user
+        {
+            get
+            {
+                return this.userField;
+            }
+            set
+            {
+                this.userField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public byte page
+        {
+            get
+            {
+                return this.pageField;
+            }
+            set
+            {
+                this.pageField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public byte perPage
+        {
+            get
+            {
+                return this.perPageField;
+            }
+            set
+            {
+                this.perPageField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public byte totalPages
+        {
+            get
+            {
+                return this.totalPagesField;
+            }
+            set
+            {
+                this.totalPagesField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public ushort total
+        {
+            get
+            {
+                return this.totalField;
+            }
+            set
+            {
+                this.totalField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class lfmLovedtracksTrack
+    {
+
+        private string nameField;
+
+        private string mbidField;
+
+        private string urlField;
+
+        private lfmLovedtracksTrackDate dateField;
+
+        private lfmLovedtracksTrackArtist artistField;
+
+        private lfmLovedtracksTrackImage[] imageField;
+
+        private lfmLovedtracksTrackStreamable streamableField;
+
+        /// <remarks/>
+        public string name
+        {
+            get
+            {
+                return this.nameField;
+            }
+            set
+            {
+                this.nameField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string mbid
+        {
+            get
+            {
+                return this.mbidField;
+            }
+            set
+            {
+                this.mbidField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string url
+        {
+            get
+            {
+                return this.urlField;
+            }
+            set
+            {
+                this.urlField = value;
+            }
+        }
+
+        /// <remarks/>
+        public lfmLovedtracksTrackDate date
+        {
+            get
+            {
+                return this.dateField;
+            }
+            set
+            {
+                this.dateField = value;
+            }
+        }
+
+        /// <remarks/>
+        public lfmLovedtracksTrackArtist artist
+        {
+            get
+            {
+                return this.artistField;
+            }
+            set
+            {
+                this.artistField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("image")]
+        public lfmLovedtracksTrackImage[] image
+        {
+            get
+            {
+                return this.imageField;
+            }
+            set
+            {
+                this.imageField = value;
+            }
+        }
+
+        /// <remarks/>
+        public lfmLovedtracksTrackStreamable streamable
+        {
+            get
+            {
+                return this.streamableField;
+            }
+            set
+            {
+                this.streamableField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class lfmLovedtracksTrackDate
+    {
+
+        private uint utsField;
+
+        private string valueField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public uint uts
+        {
+            get
+            {
+                return this.utsField;
+            }
+            set
+            {
+                this.utsField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlTextAttribute()]
+        public string Value
+        {
+            get
+            {
+                return this.valueField;
+            }
+            set
+            {
+                this.valueField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class lfmLovedtracksTrackArtist
+    {
+
+        private string nameField;
+
+        private string mbidField;
+
+        private string urlField;
+
+        /// <remarks/>
+        public string name
+        {
+            get
+            {
+                return this.nameField;
+            }
+            set
+            {
+                this.nameField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string mbid
+        {
+            get
+            {
+                return this.mbidField;
+            }
+            set
+            {
+                this.mbidField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string url
+        {
+            get
+            {
+                return this.urlField;
+            }
+            set
+            {
+                this.urlField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class lfmLovedtracksTrackImage
+    {
+
+        private string sizeField;
+
+        private string valueField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string size
+        {
+            get
+            {
+                return this.sizeField;
+            }
+            set
+            {
+                this.sizeField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlTextAttribute()]
+        public string Value
+        {
+            get
+            {
+                return this.valueField;
+            }
+            set
+            {
+                this.valueField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class lfmLovedtracksTrackStreamable
+    {
+
+        private byte fulltrackField;
+
+        private byte valueField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public byte fulltrack
+        {
+            get
+            {
+                return this.fulltrackField;
+            }
+            set
+            {
+                this.fulltrackField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlTextAttribute()]
+        public byte Value
+        {
+            get
+            {
+                return this.valueField;
+            }
+            set
+            {
+                this.valueField = value;
+            }
+        }
+    }
+
+}
---
+++ LastFmRecommendations/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace LastFmRecommendations
+{
+    //
+    // Could hand roll our own classes that we would derserailize the lastfm XML into but ..
+    // there is a much easier way.  Visual Studio provides a mechanish to turn XML into 
+    // serialize/deserialize ready classes.  See LastFm.cs for details.
+    //
+
+    class Program
+    {
+        static IsolatedStorageFile UserApplicationDirectory;
+
+
+        static XDocument createXDoc(Stream stream, string xmlFile)
+        {
+            //string getLovedTracksUri =
+            //    @"http://ws.audioscrobbler.com/2.0/" +
+            //    @"?method=user.getlovedtracks" +
+            //    @"&user=bnwest" +
+            //    @"&api_key=4eb95b1467e9cd940bd01b452a07dc98";
+            //XDocument xdoc = XDocument.Load(getLovedTracksUri);
+
+            // LINQ to XML technique:
+            XDocument xdoc = XDocument.Load(stream);
+
+            XElement root = xdoc.Root;
+
+            int numLovedTracks;
+            numLovedTracks = root.Descendants("track").Count();
+
+            //<track>
+            //  <name>No Reason To Stay</name>
+            //  <mbid></mbid>
+            //  <url>https://www.last.fm/music/Joanne+Shaw+Taylor/_/No+Reason+To+Stay</url>
+            //  <date uts="1489425111">13 Mar 2017, 17:11</date>
+            //  <artist>
+            //    <name>Joanne Shaw Taylor</name>
+            //    <mbid>1b3779c0-7414-465a-b811-a84f07131b89</mbid>
+            //    <url>https://www.last.fm/music/Joanne+Shaw+Taylor</url>
+            //  </artist>
+            //  <image size="small">https://lastfm-img2.akamaized.net/i/u/34s/51dac8d22dca485fa6bf482dad5fb5da.png</image>
+            //  <image size="medium">https://lastfm-img2.akamaized.net/i/u/64s/51dac8d22dca485fa6bf482dad5fb5da.png</image>
+            //  <image size="large">https://lastfm-img2.akamaized.net/i/u/174s/51dac8d22dca485fa6bf482dad5fb5da.png</image>
+            //  <image size="extralarge">https://lastfm-img2.akamaized.net/i/u/300x300/51dac8d22dca485fa6bf482dad5fb5da.png</image>
+            //  <streamable fulltrack="0">0</streamable>
+            //</track>
+
+#if DEBUG
+            foreach ( XElement track in root.Descendants("track") )
+            {
+                string trackName = (String) track.Element("name") ?? "";
+                string trackMBId = (string) track.Element("mbid") ?? "";
+                XElement artist = track.Element("artist");
+                string artistName = (string)artist.Element("name") ?? "";
+                string artistMBId = (string)artist.Element("mbid") ?? "";
+
+                Console.WriteLine($"Track({trackName},{trackMBId}) Artist({artistName},{artistMBId}).");
+            }
+#endif
+            //
+            // write out LastFm xml
+            //
+
+            // FileMode.Create => create a new file or truncate/overwrite an existing file
+            // serializing during a file write => get the xml declaration you expect
+
+            using ( IsolatedStorageFileStream isoStream = new IsolatedStorageFileStream(xmlFile, FileMode.Create, UserApplicationDirectory) )
+            {
+                using ( StreamWriter writer = new StreamWriter(isoStream) )
+                {
+                    xdoc.Save(isoStream);
+                }
+            }
+
+            //Console.WriteLine(xdoc.ToString()); // creates a UTF-16 string for the XML with an explicit UTF-16 encoding
+
+            return xdoc;
+        }
+
+
+        static void Main(string[] args)
+        {
+            //
+            // Going to write the XML return from lastfm.com to an xml file located in the app's Isoated Storage.
+            //
+            // IsolatedStorage for user + application (aka assembly):
+            // user\AppData\Local\IsolatedStorage\c4a3baxt.uf4\1zfwuzwr.zbj\Url.3q4wuvqyhepg5buzfu0rgckwjx3mkj3q\AssemFiles\ or some such
+            //
+
+            UserApplicationDirectory =
+              IsolatedStorageFile.GetStore(IsolatedStorageScope.User | IsolatedStorageScope.Assembly, null, null);
+
+            //
+            // as an exercise query lastfm for a set of loved tracks.
+            // populate an XDocument instance using the XML lastfm returns and
+            // populate class instance(s) also using the XML lastfm retruns.
+            //
+
+            XDocument xdoc;
+
+            LovedTracks.lfm lastFm;
+            //LovedTracks.lfmLovedtracks lovedTracks;
+
+            //
+            // create XDocument (similar to W3C XDOM) given an uri.
+            //
+
+            string getLovedTracksUri =
+                @"http://ws.audioscrobbler.com/2.0/" +
+                @"?method=user.getlovedtracks" +
+                @"&user=bnwest" +
+                @"&api_key=4eb95b1467e9cd940bd01b452a07dc98";
+
+            using ( MemoryStream memoryStream = new MemoryStream() )
+            {
+                WebClient webClient = new WebClient();
+                using ( Stream webStream = webClient.OpenRead(getLovedTracksUri) )
+                {
+                    webStream.CopyTo(memoryStream);
+                }
+
+                string localXmlFile = "lastfm.xml"; // will be created in the Isolate Storage directory
+                memoryStream.Position = 0;
+                xdoc = createXDoc(memoryStream, localXmlFile);
+
+                //
+                // populate class instance(s) with XML
+                //
+
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(LovedTracks.lfm));
+                memoryStream.Position = 0;
+                lastFm = (LovedTracks.lfm) xmlSerializer.Deserialize(memoryStream);
+
+                //WebRequest webRequest = WebRequest.Create(getLovedTracksUri);
+                //using ( WebResponse webResponse = webRequest.GetResponse() )
+                //{
+                //    using ( Stream stream = webResponse.GetResponseStream() )
+                //    {
+                //        lastFm = (LovedTracks.lfm) xmlSerializer.Deserialize(stream);
+                //        // or
+                //        using ( StreamReader readStream = new StreamReader(stream, Encoding.UTF8) )
+                //        {
+                //            lastFm = (LovedTracks.lfm) xmlSerializer.Deserialize(readStream);
+                //        }
+                //    }
+                //}
+            }
+
+            return;
+
+        }
+    }
+}
